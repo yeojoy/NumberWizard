c4bfabe8138888482829a1ce6ad70a8712c5f169
yeojoy <yeojoy.kim@gmail.com>
2020-12-24 11:08:21 -0500

    push my first game Number Wizard
    I made this game with unity. Its my first game.

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int maxNumber;
+    [SerializeField] int minNumber;
+    [SerializeField] TextMeshProUGUI guessNumberField;
+    int guessNumber;
+    private int tryCount;
+
+    // Start is called before the first frame update
+    void Start() {
+        //maxNumber += 1;
+        tryCount = 0;
+        NextGuess();
+    }
+
+    public void OnPressHigh() {
+        minNumber = guessNumber;
+        NextGuess();
+    }
+
+    public void OnPressLow() {
+        maxNumber = guessNumber;
+        NextGuess();
+    }
+
+    void NextGuess() {
+
+        if (maxNumber <= minNumber) {
+            return;
+        }
+
+        //guessNumber = (maxNumber + minNumber) / 2;
+        guessNumber = Random.Range(minNumber, maxNumber + 1);
+        guessNumberField.text = guessNumber.ToString();
+
+        tryCount++;
+        Debug.Log("Max and Min numbers are " + maxNumber + " / " + minNumber);
+        Debug.Log("User tries it " + tryCount + " time(s). The guess number is " + guessNumber);
+    }
+
+}
---
+++ Assets/Scripts/SceneLoadManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour {
+
+    public void MoveNextScene() {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void MoveFirstScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
